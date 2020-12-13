99c22886073cbe26dafda1cfe32e9ae6e141df3b
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2020-12-13 18:48:06 +0100

    added a RestResource path

--- Online-Bookstore/src/main/java/com/subhash/bookstore/repository/BookRepository.java
+++ Online-Bookstore/src/main/java/com/subhash/bookstore/repository/BookRepository.java
@@ -1,6 +1,13 @@
 package com.subhash.bookstore.repository;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.subhash.bookstore.entity.Book;
 
@@ -8,6 +15,9 @@ import com.subhash.bookstore.entity.Book;
  * @author Subhash
  *
  */
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
-
+	
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
